cf0ccab27702a0c4ea48fdb3d3049d317fc10b2b
Vanessa <vanessajmeraz@gmail.com>
2024-09-26 13:25:38 -0400

    sim integrator

--- af2rave/simulation.py
+++ af2rave/simulation.py
@@ -3,10 +3,18 @@ The simulation module for AF2RAVE performs molecular dynamics simulations.
 This module is mostly a wrapper around OpenMM, and provides utilies that create
 simulation boxes, run simulations, and analyze trajectories.
 '''
+import os
+import sys
+from sys import stdout
+import pickle
 
+from mdtraj.reporters import XTCReporter
+
+import openmm
 import openmm.app as app
 from openmm.unit import angstroms, picoseconds, kelvin
 
+import torch
 import numpy as np
 
 
@@ -110,10 +118,10 @@ class CVReporter(object):
         :type reportInterval: int
         :param list_of_indexes: The list of indexes to calculate the CVs. Default: None
         :type list_of_indexes: list[tuple[int, int]]
-        :param append: Append to existing file
-        :type append: bool
-        '''
-
+        :param append: Append to existing file 
+	    :type append: bool
+	    '''
+        
         self._out = open(file, 'a' if append else 'w')
         self._reportInterval = reportInterval
         self.list_of_cv = list_of_indexes
@@ -151,41 +159,91 @@ class UnbiasedSimulation():
     Then throw this 3-line python script to a cluster.
     '''
 
-    # What would you want to store in the object?
-    def __init__():
-        pass
-
-    def _get_system_integrator(topology,
-                               forcefield,
-                               temp: int = 310,
-                               dt: float = 0.002,
-                               cutoff: float = 10.0) -> app.Simulation:
+    
+    def __init__(self, pdb_file,
+                forcefield,
+                list_of_indexes,
+                temp: int = 310,
+                pressure: int = 1,
+                dt: float = 0.002,
+                cutoff: float = 10.0,
+                steps: int = 50000000,
+                **kwargs,
+                ):
         '''
-        Create the integrator for the system using LangevinMiddleIntegrator.
+        Simulation parameters
+        
+        :param pdb_file: Path to OpenMM.app.pdbfile.PDBFile object
+        :type pdb_file: str
+        :param forcefield: OpenMM.app.ForceField object
+        :type forcefield: OpenMM.app.ForceField
+        :param list_of_indexes: List of indexes to calculate the CVs
+        :type list_of_indexes: list[tuple[int, int]]
+        :param temp: Temperature of the system. Default: 310 K
+        :type temp: float
+        :param pressure: Pressure of the system. Default: 1 Bar
+        :type pressure: float
+        :param dt: Time step of the simulation. Default: 0.002 ps
+        :type dt: float
+        :param cutoff: Nonbonded cutoff. Default: 10.0 Angstroms
+        :type cutoff: float
+        :param steps: Simulation steps. Default:  50 million steps (100 ns)
+        :type steps: int
+        :param cv_file: File to write CVs to. Default: COLVAR.dat
+        :type cv_file: str
+        :param reportInterval: The interval at which to write the CVs. Default: 100
+        :type reportInterval: int
+        :param out_filename: File to write output XTC trajectory file to. Default: traj.xtc
+        :type out_filename: str
+        :param out_freq: Frequency of systems' state written to a trajectory file. Default: 1000
+        :type out_freq: int
+        :param append: Appends to existing file. Default: False
+        :type append: bool
+        
+        '''
+        
+        self.list_of_indexes = list_of_indexes
+        
+        self.pdb_file = app.pdbfile.PDBFile(pdb_file)
+        self.positions = self.pdb_file.positions
+        self.topology = self.pdb_file.topology
+        
+        self.forcefield = forcefield
+        self.temp = temp
+        self.pressure = pressure
+        self.dt = dt
+        self.cutoff = cutoff
+        self.steps = steps
+
+        # Reporter parameters
+        self.cv_file = kwargs.get('cv_file', "COLVAR.dat")
+        self.reportInterval = kwargs.get('reportInterval', 100)
+        self.out_filename = kwargs.get('out_filename', "traj.xtc")
+        self.out_freq = kwargs.get('out_freq', 1000)
+        self.append = kwargs.get('append', False)
+        
+    def _get_system_integrator(self) -> app.Simulation:
+        '''
+        Create the integrator for the system using LangevinMiddleIntegrator. 
         Finds the CUDA platform if available and will fallback to CPU if not.
         Returns the OpenMM simulation object.
 
-        :param topology: OpenMM.app.Topology object
-        :type topology: OpenMM.app.Topology
-        :param forcefield: OpenMM.app.ForceField object
-        :type forcefield: OpenMM.app.ForceField
-        :param temp: Temperature of the system. Default: 310 K
-        :type temp: int
-        :param dt: Time step of the simulation. Default: 0.002 ps
-        :type dt: float
-        :param cutoff: Nonbonded cutoff. Default: 10.0 Angstrom
-        :type cutoff: float
         '''
-        pass
+        # finds device
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+        system = self.forcefield.createSystem(self.topology,
+                                              nonbondedMethod=app.PME,
+                                              nonbondedCutoff=self.cutoff*angstroms,
+                                              constraints=app.HBonds)
+                                              
+        integrator = openmm.LangevinMiddleIntegrator(self.temp*kelvin, 1/picoseconds, self.dt*picoseconds)
 
-    def run(steps: int = 50000000):
-        '''
-        Run the simulation from given pdb file. Default: 50 million steps (100 ns).
-        '''
-        pass
-
-    def restart():
-        '''
-        Restart the simulation with a given PDB and checkpoint file.
-        '''
-        pass
+        if device.type == "cpu":
+            simulation = app.Simulation(self.topology, system, integrator)
+        else:
+            platform = openmm.Platform.getPlatformByName("CUDA")
+            simulation = app.Simulation(self.topology, system, integrator, platform)
+            
+        return simulation
+            
