bbc48263c3f748f84a2209e24ac40e9d38928c0e
Da Teng <tengda1997@gmail.com>
2024-11-05 15:13:44 -0500

    lint tests

--- tests/test_colvar.py
+++ tests/test_colvar.py
@@ -6,6 +6,7 @@ import os
 DAT_LENGTH = 1001
 DAT_COL = 428
 
+
 def check_reproduce(colvar):
     colvar.write("./tests/test_colvars/test.dat")
     new_colvar = Colvar.from_file("./tests/test_colvars/test.dat")
@@ -14,18 +15,22 @@ def check_reproduce(colvar):
     assert np.allclose(new_colvar._data, colvar._data)
     os.remove("./tests/test_colvars/test.dat")
 
+
 def test_import():
     assert Colvar is not None
+
 
 def test_load():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
     assert colvar is not None
     assert colvar._data.shape == (DAT_COL, DAT_LENGTH)
 
+
 def test_missing_metadata():
     with pytest.raises(ValueError):
         colvar = Colvar.from_file("./tests/test_colvars/missing_meta_data.dat")
         assert colvar is None
+
 
 def test_shape():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
@@ -33,15 +38,18 @@ def test_shape():
     assert colvar.shape == (len(colvar.header), len(colvar._data[0]))
     assert colvar.shape == colvar._data.shape
 
+
 def test_header():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
     assert colvar.header == colvar._header
     assert colvar.header[0:3] == ["d5_1664", "d5_1665", "d5_1666"]
     assert len(colvar.header) == DAT_COL
 
+
 def test_write():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
     check_reproduce(colvar)
+
 
 def test_choose():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
@@ -50,11 +58,13 @@ def test_choose():
     assert colvar2.shape == (3, DAT_LENGTH)
     check_reproduce(colvar2)
 
+
 def test_choose_invariance():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
     colvar2 = colvar.choose(["d5_1664", "d5_1665", "d5_1666"])
     del colvar["d5_1664"]
     assert colvar2["d5_1664"] is not None
+
 
 def test_choose_invariance2():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
@@ -62,17 +72,20 @@ def test_choose_invariance2():
     colvar["d5_1664"] = np.ones(DAT_LENGTH)
     assert not np.allclose(colvar2["d5_1664"], np.ones(DAT_LENGTH))
 
+
 def test_tappend():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
     colvar.tappend(colvar)
     assert colvar.shape == (DAT_COL, 2 * DAT_LENGTH)
     check_reproduce(colvar)
 
+
 def test_tappend_with_shorter():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
     colvar2 = colvar.choose(["d5_1664", "d5_1665", "d5_1666"])
     with pytest.raises(ValueError):
         colvar.tappend(colvar2)
+
 
 def test_tappend_with_longer():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
@@ -81,19 +94,23 @@ def test_tappend_with_longer():
     assert colvar2.shape == (3, 2 * DAT_LENGTH)
     check_reproduce(colvar2)
 
+
 def test_kappend():
     pass
+
 
 def test_magic_contains():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
     assert "d5_1664" in colvar
     assert "pratyush" not in colvar
 
+
 def test_magic_getitem():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
     assert np.allclose(colvar["d5_1664"], colvar._data[0])
     with pytest.raises(KeyError):
         colvar["pratyush"]
+
 
 def test_magic_setitem():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
@@ -103,6 +120,7 @@ def test_magic_setitem():
     assert colvar.shape == (DAT_COL + 1, DAT_LENGTH)
     check_reproduce(colvar)
 
+
 def test_magic_setitem_existing():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
     colvar["d5_1664"] = np.ones(DAT_LENGTH)
@@ -110,10 +128,12 @@ def test_magic_setitem_existing():
     assert colvar.shape == (DAT_COL, DAT_LENGTH)
     check_reproduce(colvar)
 
+
 def test_magic_setitem_wrong_length():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
     with pytest.raises(ValueError):
         colvar["pratyush"] = np.ones(DAT_LENGTH - 1)
+
 
 def test_magic_delitem():
     colvar = Colvar.from_file("./tests/test_colvars/base.dat")
@@ -121,5 +141,3 @@ def test_magic_delitem():
     assert "d5_1664" not in colvar
     assert colvar.shape == (DAT_COL - 1, DAT_LENGTH)
     check_reproduce(colvar)
-    
-    
--- tests/test_feature.py
+++ tests/test_feature.py
@@ -1,19 +1,22 @@
 from af2rave import feature
 import os
 
-pdb_dir = f"./tests/test_pdb/"
+pdb_dir = "./tests/test_pdb/"
 ref_pdb = f"{pdb_dir}/pred_1.pdb"
 pdb_files = [f"{pdb_dir}/{file}" for file in os.listdir(pdb_dir) if file.endswith('.pdb')]
+
 
 def test_import():
     assert feature is not None
     assert feature.Feature is not None
     assert feature.FeatureSelection is not None
 
+
 def test_init_no_ref():
     fs = feature.FeatureSelection(pdb_files)
     assert fs.traj.n_frames == 111
     assert fs.ref_pdb is not None
+
 
 def test_init_with_ref():
     fs = feature.FeatureSelection(pdb_files, ref_pdb)
@@ -22,9 +25,11 @@ def test_init_with_ref():
     assert fs.traj is not None
     assert fs.ref is not None
 
+
 def test_init_with_dir():
     fs = feature.FeatureSelection(pdb_dir)
     assert fs.ref_pdb.endswith("pred_1.pdb")
+
 
 class TestProperties:
     '''
