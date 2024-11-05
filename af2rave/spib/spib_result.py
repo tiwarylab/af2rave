f26ba319dab4c3faa787dcfb5d2785dadf2e4ad1
Da Teng <tengda1997@gmail.com>
2024-11-05 12:15:25 -0500

    add PMF calculation

--- af2rave/spib/spib_result.py
+++ af2rave/spib/spib_result.py
@@ -64,20 +64,40 @@ class SPIBResult():
         return self._dt
     
     @property
+    def n_traj(self):
+        return self._n_traj
+    
+    @property
     def n_input_labels(self):
         return self._traj[0]["labels"].shape[1]
     
     @property
-    def n_converged_states(self):
+    def n_converged_states(self) -> int:
+        '''
+        The number of remaining converged states.
+        '''
         return np.sum(self._converged_states)
     
-    def _get_converged_states(self):
+    def _get_converged_states(self) -> np.ndarray:
+        '''
+        Return a one-hot encoding of all remaining states.
+
+        :return label: The one-hot encoding of the remaining states. Dimension: n_input_labels
+        :rtype: np.ndarray
+        '''
+
         labels = np.zeros(self.n_input_labels, dtype=int)
         for i in np.arange(self._n_traj):
             labels |= np.any(self._traj[i]["labels"], axis=0)
         return labels
     
     def project(self, X):
+        '''
+        Project the input data to the latent space.
+
+        :param X: The input data to project. Dimension: n_input_dims x n_frames
+        :type X: np.ndarray
+        '''
 
         # shape of X: n_input_dims x n_frames
         # shape of weight: 2 x n_input_dims
@@ -89,6 +109,7 @@ class SPIBResult():
         else:
             b, k = 0, 1
             print("[SPIBResult.project] Missing min-max scaling information.")
+
         p = (X - b) / k
         p = np.dot(self._z_mean_encoder["weight"], p) + self._z_mean_encoder["bias"]
         return p
@@ -108,4 +129,65 @@ class SPIBResult():
         Z.map(projection)
 
         return Z
+    
+    def get_latent_representation(self, traj_idx: int = None):
+        '''
+        Return the latent representation of the trajectory. 
+        If no index is provides, return all trajectories.
+        '''
 
+        if traj_idx is not None:
+            rep = self._traj[traj_idx]["representation"]
+        else:
+            rep = np.vstack([traj["representation"] for traj in self._traj])
+        return rep.T
+
+    def get_state_label(self, traj_idx: int = None):
+        '''
+        Return the state label of the trajectory. 
+        If no index is provides, return all trajectories.
+        '''
+
+        if traj_idx is not None:
+            state = self._traj[traj_idx]["labels"].nonzero()[1]
+        else:
+            state = np.hstack([traj["labels"].nonzero()[1] for traj in self._traj])
+        return state
+    
+    def get_traj_label(self, traj_idx: int = None):
+        '''
+        Return the trajectory label of the trajectory. 
+        If no index is provides, return all trajectories.
+        '''
+
+        if traj_idx is not None:
+            traj = np.full(self._traj[traj_idx]["labels"].shape[0], traj_idx)
+        else:
+            traj = np.hstack([np.full(traj["labels"].shape[0], i) for i, traj in enumerate(self._traj)])
+        return traj
+    
+    def get_probability_distribution(self, nbins = 200):
+
+        h, x, y = np.histogram2d(*self.get_latent_representation(), bins=nbins, density=True)
+        return x, y, h
+    
+    def get_free_energy(self, nbins = 200):
+        '''
+        Get the free energy as the negative logarithm of the probability distribution. Unit: kT
+
+        :param nbins: The number of bins for the histogram. The format is the same with np.histogram2d.
+        :type nbins: int, (int, int), optional, default=200
+        :return: x, y, f
+
+        Example:
+        - Plot in matplotlib
+
+        `plt.pcolor(*result.get_free_energy(), cmap="RdBu_r", shading="auto")`
+
+        The sequence of the return value allows a direct input to the pcolor function.
+        '''
+
+        x, y, h = self.get_probability_distribution(nbins)
+        f = -np.log(h)
+        f -= np.min(f)
+        return x, y, f
