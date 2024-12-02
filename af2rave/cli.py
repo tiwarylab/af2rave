d3a140004cb04541a31a6f8cc376d7fd3d1f7d47
Da Teng <tengda1997@gmail.com>
2024-12-02 16:01:48 -0500

    simulation cli

--- af2rave/cli.py
+++ af2rave/cli.py
@@ -1,5 +1,6 @@
 """Console script for af2rave."""
 import af2rave
+from . import af2rave as wrapper
 
 import typer
 from rich.console import Console
@@ -14,7 +15,18 @@ def main():
     console.print("Replace this message by putting your code into "
                "af2rave.cli.main")
     console.print("See Typer documentation at https://typer.tiangolo.com/")
-    
+
+@app.command()
+def simulation(input_json: str):
+    """
+    Run the MD part of af2rave.
+    """
+    input = wrapper.parse_input(input_json)
+    sim_metadata = input.get("simulation", None)
+    if sim_metadata is None:
+        console.print("No simulation metadata found in the input JSON.")
+        return -1
+    return wrapper.simulation(sim_metadata)
 
 if __name__ == "__main__":
     app()
